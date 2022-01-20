7ad115c53ee770b13461a6c628ed9d0e931d5402
anvorgesa <aiuztina@gmail.com>
2022-01-20 10:41:47 -0300

    (Fix) MessageFormatter: scan arrays

--- src/main/java/eu/mcdb/ban_announcer/MessageFormatter.java
+++ src/main/java/eu/mcdb/ban_announcer/MessageFormatter.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.spicord.embed.Embed;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
@@ -39,27 +40,36 @@ public class MessageFormatter {
 
         JsonObject root = gson.toJsonTree(embed).getAsJsonObject();
 
-        formatObject(root);
+        formatElement(root);
 
         return gson.fromJson(root, Embed.class);
     }
 
-    private void formatObject(JsonObject element) {
-        for (Map.Entry<String, JsonElement> e : element.entrySet()) {
-            String key = e.getKey();
-            JsonElement value = e.getValue();
+    private void formatElement(JsonElement element) {
+        if (element.isJsonObject()) {
+            JsonObject object = element.getAsJsonObject();
 
-            if (isString(value)) {
-                boolean isUrl = "url".equals(key) || "icon_url".equals(key);
+            for (Map.Entry<String, JsonElement> entry : object.entrySet()) {
+                String key = entry.getKey();
+                JsonElement value = entry.getValue();
 
-                String val = getAsString(value);
+                if (isString(value)) {
+                    boolean isUrl = "url".equals(key) || "icon_url".equals(key);
 
-                val = setPlaceholders(val, isUrl);
+                    String val = getAsString(value);
 
-                element.addProperty(key, val);
+                    val = setPlaceholders(val, isUrl);
 
-            } else if (value.isJsonObject()) {
-                formatObject(value.getAsJsonObject());
+                    object.addProperty(key, val);
+                } else {
+                    formatElement(value);
+                }
+            }
+        } else if (element.isJsonArray()) {
+            JsonArray array = element.getAsJsonArray();
+
+            for (int i = 0; i < array.size(); i++) {
+                formatElement(array.get(i));
             }
         }
     }
